1436932699e169cd3f39852ffa99e3d08acb60f0
Luis Fernando Arevalo Antezana <clamth01@gmail.com>
2020-10-28 11:46:11 -0400

    fix login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -13,35 +13,56 @@ const { Content } = Layout;
 
 const index = () => {
   const [logged, setLogged] = useState(false);
-  const router = useRouter();  
+  const router = useRouter();
 
   const goContainerSignIn = () => {
     router.push("/signIn");
   };
 
-  const doLogin = (values) => {
-    setLogged(true);
-    if (logged == true) {
+  const doLogin = async (inputs) => {
+    const url = "https://dbcolegios.blockchainconsultora.com/users/login";
+    const form = {
+      email: inputs.email,
+      password: inputs.password,
+      societyId: "fafc7359-0139-43d7-9d00-1828d90cc407",
+    };
+    const option = {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const request = await fetch(url, option)
+      .then((values) => values.json())
+      .catch((err) => err.json());
+    
+    if(request.token ){
       notification["success"]({
-        message: "WOW!!",
+        message: "Iniciaste secion",
         style: {
           width: 600,
         },
-        description: values.email + "  haz iniciado sesion en FatCat...",
+        description: `Iniciaste sesion y tu token es: ${request.token}`,
+      });
+    }else{
+      notification["warning"]({
+        message: "Error de usuario o clave sesion",
+        style: {
+          width: 600,
+        },
+        description: "Error al iniciar sesion",
       });
     }
   };
-
-  useEffect(() => {
-    console.log("estas Login");
-  }, [])
 
   return (
     <Layout style={{ height: "100%", width: "100%", justifyContent: "center" }}>
       <Content style={{ paddingTop: "11%", paddingBottom: "10%" }}>
         <Row>
           <Col offset={4}>
-            <img src="/assets/cat2.png" alt="loading"/>
+            <img src="/assets/cat2.png" alt="loading" />
           </Col>
           <Col span={6} offset={1}>
             <div style={{ paddingBottom: "10%" }}>
@@ -76,10 +97,9 @@ const index = () => {
             </Form>
           </Col>
         </Row>
-      </Content>      
+      </Content>
     </Layout>
   );
 };
 
 export default index;
-
